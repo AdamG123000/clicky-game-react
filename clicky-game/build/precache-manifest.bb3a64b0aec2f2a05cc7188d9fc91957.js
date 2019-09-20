41eebb9d1f374d92f8ce0bdab38dd6da97ba34ed
Adam Grodahl <adamgrodahl@Adams-MBP.hsd1.or.comcast.net>
2019-09-20 16:37:24 -0700

    operational clicky-game

---
+++ clicky-game/build/precache-manifest.bb3a64b0aec2f2a05cc7188d9fc91957.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc49f405292683b0544bbad2c1191028",
+    "url": "/index.html"
+  },
+  {
+    "revision": "27f3656a3bd57c7330b1",
+    "url": "/static/css/main.5ed28704.chunk.css"
+  },
+  {
+    "revision": "e182e26e9970f35735de",
+    "url": "/static/js/2.90079b64.chunk.js"
+  },
+  {
+    "revision": "27f3656a3bd57c7330b1",
+    "url": "/static/js/main.0a086895.chunk.js"
+  },
+  {
+    "revision": "d852e20e7dcfe92bb79a",
+    "url": "/static/js/runtime-main.e2af47e7.js"
+  }
+]);
